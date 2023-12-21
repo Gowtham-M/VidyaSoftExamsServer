8e76d9f0531bf4b8c6d063cd66d24bbd8a1e0138
kotanagasruthi <nagasruthikota@gmail.com>
2023-12-21 11:34:59 +0530

    API cors error and sending time to UI

--- routes/exam-results.js
+++ routes/exam-results.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
+
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+const cors = require('cors');
+router.use(cors({
+  origin: 'http://localhost:8080', // Replace with the exact URL of your Vue.js frontend
+  credentials: true
+}));
+
 const Question = require('../models/questions');
 const ExamResult = require('../models/exam-results');
 
-router.post('/insertcandidate', async (req, res) => {
+router.post('/insertCandidate', async (req, res) => {
     try {
         console.log("req.body", req.body)
         const { exam_id, candidate_email } = req.body;
@@ -20,19 +31,20 @@ router.post('/insertcandidate', async (req, res) => {
             });
         }
         // Create a document in the exam-results collection
+        console.log('date now', Date.now())
         const examResult = new ExamResult({
             exam_id,
             candidate_email,
             candidate_questions,
             exam_status: 'pending', // Set the initial exam status
-            start_time: new Date(), // Set the start time
+            start_time: Date.now(), // Set the start time
             completion_time: null, // Set the completion time as null initially
             score: 0, // Set the initial score as 0
         });
 
         await examResult.save();
 
-        res.status(201).json({ message: 'Exam result created successfully' });
+        res.status(200).json({ startTime: Date.now(), message: 'Exam result created successfully' });
     } catch (error) {
         res.status(500).json({ message: 'Failed to create exam result' });
     }
