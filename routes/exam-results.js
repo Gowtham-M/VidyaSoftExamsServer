b23329150fe85c8e94e044b02dbe1683c81b9630
Gowtham-M <43465052+Gowtham-M@users.noreply.github.com>
2023-12-22 22:29:14 +0530

    added score

--- routes/exam-results.js
+++ routes/exam-results.js
@@ -12,11 +12,13 @@ router.post('/insertcandidate', async (req, res) => {
         const questions = await Question.find({exam_id:exam_id});
 
         for (let question of questions) {
+            console.log("question", question)
             candidate_questions.push({
                 question: question.question_text,
                 question_ans: question.correct_answer,
                 answer_given: '',
-                status: 'not answered'
+                status: 'not answered',
+                question_marks: question.marks,
             });
         }
         // Create a document in the exam-results collection
@@ -27,7 +29,8 @@ router.post('/insertcandidate', async (req, res) => {
             exam_status: 'pending', // Set the initial exam status
             start_time: new Date(), // Set the start time
             completion_time: null, // Set the completion time as null initially
-            score: 0, // Set the initial score as 0
+            score: 0,
+            question_score:"" // Set the initial score as 0
         });
 
         await examResult.save();
@@ -72,6 +75,37 @@ router.put('/updatequestionstatus', async (req, res) => {
         res.status(500).json({ message: 'Failed to update question status' });
     }
 });
+
+router.get('/fetchcandidate', async (req, res) => {
+    try {
+        const { exam_id, candidate_email } = req.query;
+
+        // Find the exam result document by exam_id and candidate_email
+        const examResult = await ExamResult.findOne({ exam_id, candidate_email });
+
+        if (!examResult) {
+            return res.status(404).json({ message: 'Exam result not found' });
+        }
+
+        let score = 0;
+
+        // Calculate the score by comparing question_answer to answer_given
+        for (let question of examResult.candidate_questions) {
+            if (question.question_ans === question.answer_given) {
+                score += question.question_marks;
+            }
+        }
+
+        examResult.score = score;
+        await examResult.save();
+
+        res.status(200).json(examResult);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch candidate details' });
+    }
+});
+
+
 
 
 module.exports = router;
